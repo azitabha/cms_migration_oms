66ae0ec52125691068b6a790b96a61e5283a5601
azitabh <azitabh@azitabh.(none)>
2012-07-19 07:58:59 +0530

    commit

--- migrator.sql
+++ migrator.sql
@@ -4,5 +4,5 @@ insert Order_new select * from `Order`;
 alter table Order_new change nid nid varchar(20) not null default '0';
 update Escrow_tp.Order_new o inner join tradusclub.PRODUCTID_NID_MAPPING p on o.nid=p.nid and o.AuctionId=p.seller_sku set o.nid = p.product_id where p.product_id is not null;
 rename table `Order` to Order_old;
-rename table `Order_new` to Order;
+rename table `Order_new` to `Order`;
 
